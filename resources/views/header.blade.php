e8e42400e2cc72de55885d8df190e23df802f30e
Jovan Popovic <jovan.popovic@outlook.com>
2021-01-26 17:10:49 +0100

    Logout complete

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,12 @@
+<?php
+use App\Http\Controllers\ProductController;
+
+$total=0;
+if (Session::has('user')) {
+    $total = ProductController::cartItem();
+}
+
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -7,7 +16,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Orders</a>
@@ -17,9 +26,21 @@
         <input name="query" class="form-control me-2 search-field" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li><a href="">Cart</a></li>
-      </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li><a href="">Cart({{$total}})</a></li>
+          @if(Session::has('user'))
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {{Session::get('user')['name']}}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="/logout">Logout</a>
+            </div>
+          </li>
+           @else
+            <a href="/login">Login</a>
+           @endif
+        </ul>
     </div>
   </div>
 </nav>
